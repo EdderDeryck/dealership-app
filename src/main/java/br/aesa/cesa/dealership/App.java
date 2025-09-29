6bec3f50644bbd54bc8b7eff973fb41594342103
Gustavo <gustavodasilvama10@gmail.com>
2025-09-29 17:28:51 -0300

    feat: structure the folders following the Maven standard

---
+++ src/main/java/br/aesa/cesa/dealership/App.java
@@ -0,0 +1,13 @@
+package main.java.br.aesa.cesa.dealership;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
