51954ba97fa91ff22021852b2200392884ed7323
Edder <opecadodairadodragaomeliodas3@gmail.com>
2025-10-02 21:05:15 -0300

    Create Person.java

---
+++ src/main/java/br/aesa/cesa/dealership/model/Person.java
@@ -0,0 +1,36 @@
+package main.java.br.aesa.cesa.dealership.person;
+
+public abstract class Person {
+    private String name;
+    private String cpf;
+
+    public Person() {
+    }
+
+    public Person(String name, String cpf) {
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public boolean validateCpf() {
+        return this.cpf != null && this.cpf.length() == 11;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public abstract void introduce();
+}
