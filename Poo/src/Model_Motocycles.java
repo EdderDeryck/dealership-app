4cef0f1eed0d51a81d0f2f0dd6060d83e737214b
Sylmar Vyctor <sylmarvyctor@gmail.com>
2025-10-06 00:12:04 -0300

    Add files via upload

---
+++ Poo/src/Model_Motocycles.java
@@ -0,0 +1,108 @@
+
+public class Model_Motocycles {
+    public static void main(String[] args) {
+
+System.out.println("Seja bem-vindo a tabela de motos disponievis para sua compra.");
+
+
+        Motocycles m1 = new Motocycles();
+
+        m1.name_Motocycles = "CG Honda Titan 160";
+        m1.model_Motocycles = "Street";
+        m1.price_Motocycles = 13.784;
+
+        m1.informar_Nome_Motocycles();
+        m1.informar_Model_Motocycles();
+        m1.informar_Price_Motocycles();
+
+        Motocycles m2 = new Motocycles();
+
+        m2.name_Motocycles = "CG Honda Fan 160";
+        m2.model_Motocycles = "Street";
+        m2.price_Motocycles = 15.886;
+
+        m2.informar_Nome_Motocycles();
+        m2.informar_Model_Motocycles();
+        m2.informar_Price_Motocycles();
+
+        Motocycles m3 = new Motocycles();
+
+        m3.name_Motocycles = "CG Honda Start 160";
+        m3.model_Motocycles = "Street";
+        m3.price_Motocycles = 17.999;
+
+        m3.informar_Nome_Motocycles();
+        m3.informar_Model_Motocycles();
+        m3.informar_Price_Motocycles();
+
+        Motocycles m4 = new Motocycles();
+        m4.name_Motocycles = "Honda Biz 125";
+        m4.model_Motocycles = "CUB";
+        m4.price_Motocycles = 16.987;
+
+        m4.informar_Nome_Motocycles();
+        m4.informar_Model_Motocycles();
+        m4.informar_Price_Motocycles();
+
+        Motocycles m5 = new Motocycles();
+
+        m5.name_Motocycles = "Honda Biz 110i";
+        m5.model_Motocycles = "CUB";
+        m5.price_Motocycles = 14.899;
+
+        m5.informar_Nome_Motocycles();
+        m5.informar_Model_Motocycles();
+        m5.informar_Price_Motocycles();
+
+        Motocycles m6 = new Motocycles();
+
+        m6.name_Motocycles = "Honda Pop 110i";
+        m6.model_Motocycles = "Street";
+        m6.price_Motocycles = 13.350;
+
+        m6.informar_Nome_Motocycles();
+        m6.informar_Model_Motocycles();
+        m6.informar_Price_Motocycles();
+
+        Motocycles m7 = new Motocycles();
+        m7.name_Motocycles = "Honda XRE 190";
+        m7.model_Motocycles = "Trail";
+        m7.price_Motocycles = 23.580;
+
+        m7.informar_Nome_Motocycles();
+        m7.informar_Model_Motocycles();
+        m7.informar_Price_Motocycles();
+
+        Motocycles m8 = new Motocycles();
+        m8.name_Motocycles = "Honda XRE 300";
+        m8.model_Motocycles = "Trail/Adventure";
+        m8.price_Motocycles = 34.999;
+
+        m8.informar_Nome_Motocycles();
+        m8.informar_Model_Motocycles();
+        m8.informar_Price_Motocycles();
+
+        Motocycles m9 = new Motocycles();
+        m9.name_Motocycles = "Honda CB 500F";
+        m9.model_Motocycles = "Nakked";
+        m9.price_Motocycles = 47.997;
+
+        m9.informar_Nome_Motocycles();
+        m9.informar_Model_Motocycles();
+        m9.informar_Price_Motocycles();
+
+        Motocycles m10 = new Motocycles();
+        m10.name_Motocycles = "Honda CB 650F";
+        m10.model_Motocycles = "Nakked";
+        m10.price_Motocycles = 58.279;
+
+        m10.informar_Nome_Motocycles();
+        m10.informar_Model_Motocycles();
+        m10.informar_Price_Motocycles();
+
+
+
+
+
+        }
+    }
---
+++ Poo/src/Motocycles.java
@@ -0,0 +1,22 @@
+public class Motocycles {
+
+       String name_Motocycles;
+       String model_Motocycles;
+       double price_Motocycles;
+
+       void informar_Nome_Motocycles(){
+          System.out.println("Name da Motocycles:" + name_Motocycles);
+       }
+       void informar_Model_Motocycles(){
+           System.out.println("Model da Motocycles:" + model_Motocycles);
+       }
+       void informar_Price_Motocycles(){
+           System.out.println("Price da Motocycles:" + "R$" + price_Motocycles);
+       }
+
+
+
+
+
+
+}
